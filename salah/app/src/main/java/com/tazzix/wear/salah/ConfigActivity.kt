873fe7234ffd4b1be53b897f86572d45d61ac0fb
Tasnim Ahmed <phonedev@tazzix.com>
2021-10-10 13:00:43 +0500

    switching to wearbalerecycler view based menus in config, might solve play store review issue

--- salah/app/src/main/java/com/tazzix/wear/salah/ConfigActivity.kt
+++ salah/app/src/main/java/com/tazzix/wear/salah/ConfigActivity.kt
@@ -1,48 +1,48 @@
 package com.tazzix.wear.salah
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Spinner
+import android.util.Log
 import androidx.fragment.app.FragmentActivity
 
-class ConfigActivity : FragmentActivity() {
+import androidx.wear.widget.WearableLinearLayoutManager
+import androidx.wear.widget.WearableRecyclerView
 
-    private lateinit var methodSpinner: Spinner
-    private lateinit var asrCalcSpinner: Spinner
+
+class ConfigActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
 
-        methodSpinner = findViewById(R.id.method_choice)
-        asrCalcSpinner = findViewById(R.id.asrcalc_choice)
+        val recyclerView: WearableRecyclerView = findViewById(R.id.config_menu_view)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.isEdgeItemsCenteringEnabled = true
+        recyclerView.layoutManager = WearableLinearLayoutManager(this)
 
-        // TODO: show initial chosen values as per sharedprefs
-        val sharedPreference =  getSharedPreferences("SALAH_LOCATION", Context.MODE_PRIVATE)
-        asrCalcSpinner.setSelection(sharedPreference.getInt("ASR_CALC", 0))
-        methodSpinner.setSelection(sharedPreference.getInt("METHOD", 0))
+        val menuItems: ArrayList<MenuItem> = ArrayList()
+        val sharedPreferences =  getSharedPreferences("SALAH_LOCATION", Context.MODE_PRIVATE)
+        val curMethod = resources.getStringArray(R.array.array_methods)[sharedPreferences.getInt("METHOD", 0)]
+        val curAsrCalc = resources.getStringArray(R.array.array_asrcalcs)[sharedPreferences.getInt("ASR_CALC", 0)]
+        menuItems.add(MenuItem(android.R.drawable.ic_menu_compass, getString(R.string.method), curMethod))
+        menuItems.add(MenuItem(android.R.drawable.ic_menu_info_details, getString(R.string.asr_calc), curAsrCalc))
 
-        // TODO: get selection changes
-        asrCalcSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
+        recyclerView.adapter = MenuAdapter(this, menuItems, object: MenuAdapter.AdapterCallback {
+            override fun onItemClicked(menuPosition: Int?) {
+                Log.d("SALAH", "Item: $menuPosition")
+                when (menuPosition) {
+                    0 -> {
+                        val intent = Intent(parent, ConfigActivity::class.java)
+                        startActivity(intent)
+                    }
+                    1 -> {
+                    }
+                    else -> {
+                        finish()
+                    }
+                }
             }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val sharedPreference =  getSharedPreferences("SALAH_LOCATION", Context.MODE_PRIVATE)
-                sharedPreference.edit().putInt("ASR_CALC", position).apply()
-            }
-        }
-
-        methodSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val sharedPreference =  getSharedPreferences("SALAH_LOCATION", Context.MODE_PRIVATE)
-                sharedPreference.edit().putInt("METHOD", position).apply()
-            }
-        }
+        })
     }
 }
---
+++ salah/app/src/main/java/com/tazzix/wear/salah/MenuAdapter.kt
@@ -0,0 +1,61 @@
+package com.tazzix.wear.salah
+
+import android.content.Context
+import android.widget.TextView
+import android.widget.RelativeLayout
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+
+class MenuAdapter(// private val drawableIcon: String? = null
+    private val context: Context, dataArgs: ArrayList<MenuItem>, callback: AdapterCallback?) :
+    RecyclerView.Adapter<MenuAdapter.RecyclerViewHolder>() {
+    private var dataSource = ArrayList<MenuItem>()
+
+    interface AdapterCallback {
+        fun onItemClicked(menuPosition: Int?)
+    }
+
+    private val callback: AdapterCallback?
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewHolder {
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.recview_item, parent, false)
+        return RecyclerViewHolder(view)
+    }
+
+    class RecyclerViewHolder(view: View) : ViewHolder(view) {
+        var menuContainer: RelativeLayout = view.findViewById(R.id.menu_container)
+        var menuTitle: TextView = view.findViewById(R.id.menu_title)
+        var menuDetail: TextView = view.findViewById(R.id.menu_detail)
+        var menuIcon: ImageView = view.findViewById(R.id.menu_icon)
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        val dataProvider = dataSource[position]
+        holder.menuTitle.text = dataProvider.text
+        holder.menuDetail.text = dataProvider.detail
+        holder.menuIcon.setImageResource(dataProvider.image)
+        holder.menuContainer.setOnClickListener {
+            callback?.onItemClicked(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataSource.size
+    }
+
+    init {
+        dataSource = dataArgs
+        this.callback = callback
+    }
+}
+
+class MenuItem(val image: Int, val text: String, val detail: String)
